3314969e99dd42c9b301bdf58f38f3ae904cf64f
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2021-09-12 13:29:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// // external js: flickity.pkgd.js
+
+// var $mainCarousel = $('.carousel-main').flickity({
+//     prevNextButtons: false,
+//   });
+  
+//   var $navCarousel = $('.carousel-nav').flickity({
+//     asNavFor: '.carousel-main',
+//     cellSelector: '.carousel-cell',
+//     contain: true,
+//     pageDots: false,
+//     prevNextButtons: false,
+//   });
+  
+//   $('.prev-button').on( 'click', function() {
+//     $mainCarousel.flickity('previous');
+//   });
+  
+//   $('.next-button').on( 'click', function() {
+//     $mainCarousel.flickity('next');
+//   });
